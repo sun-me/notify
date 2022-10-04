58fd0ba496d4d8de0f96c6fb1cd5c9925ddf9493
sun k <sun@sooas.com>
2022-10-04 23:56:15 +0900

    success push notify test

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:notify/model/push_notification.dart';
 import 'package:overlay_support/overlay_support.dart';
 
-void main() {
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
+}
+
+void main() async {
   runApp(MyApp());
 }
 
@@ -18,37 +23,70 @@ class MyApp extends StatelessWidget {
         ),
         debugShowCheckedModeBanner: false,
         home: HomePage(),
-      )
+      ),
     );
   }
 }
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class PushNotification {
-  PushNotification({
-    this.title,
-    this.body,
-    this.dataTitle,
-    this.dataBody,
-  });
-  String? title;
-  String? body;
-  String? dataTitle;
-  String? dataBody;
-}
-
-class _HomePageState extends State {
+class _HomePageState extends State<HomePage> {
   late final FirebaseMessaging _messaging;
   late int _totalNotifications;
   PushNotification? _notificationInfo;
 
+  void registerNotification() async {
+    await Firebase.initializeApp();
+    _messaging = FirebaseMessaging.instance;
 
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-  // 그러나 앱이 종료된 상태이고 알림을 탭하여 다시 가져온 경우 이 메서드는 정보를 검색하기에 충분하지 않습니다.
-  // 호출된 메서드를 정의하고 여기에 다음 코드를 추가합니다.initState()checkForInitialMessage()
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        print(
+            'Message title: ${message.notification?.title}, body: ${message.notification?.body}, data: ${message.data}');
+
+        // Parse the message received
+        PushNotification notification = PushNotification(
+          title: message.notification?.title,
+          body: message.notification?.body,
+          dataTitle: message.data['title'],
+          dataBody: message.data['body'],
+        );
+
+        setState(() {
+          _notificationInfo = notification;
+          _totalNotifications++;
+        });
+
+        if (_notificationInfo != null) {
+          // For displaying the notification as an overlay
+          showSimpleNotification(
+            Text(_notificationInfo!.title!),
+            leading: NotificationBadge(totalNotifications: _totalNotifications),
+            subtitle: Text(_notificationInfo!.body!),
+            background: Colors.cyan.shade700,
+            duration: Duration(seconds: 2),
+          );
+        }
+      });
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
+
   // For handling notification when the app is in terminated state
   checkForInitialMessage() async {
     await Firebase.initializeApp();
@@ -59,7 +97,10 @@ class _HomePageState extends State {
       PushNotification notification = PushNotification(
         title: initialMessage.notification?.title,
         body: initialMessage.notification?.body,
+        dataTitle: initialMessage.data['title'],
+        dataBody: initialMessage.data['body'],
       );
+
       setState(() {
         _notificationInfo = notification;
         _totalNotifications++;
@@ -70,53 +111,12 @@ class _HomePageState extends State {
   @override
   void initState() {
     _totalNotifications = 0;
+    registerNotification();
+    checkForInitialMessage();
 
-    // 앱이 백그라운드에 있고 알림을 탭할 때 작업을 처리하려면 메서드 에 다음 코드를 추가해야 합니다.initState()
     // For handling notification when the app is in background
     // but not terminated
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      PushNotification notification = PushNotification(
-        title: message.notification?.title,
-        body: message.notification?.body,
-      );
-      setState(() {
-        _notificationInfo = notification;
-        _totalNotifications++;
-      });
-    });
-    // end add onMessageOpenedApp.listen
-    // Call here
-    checkForInitialMessage();
-
-    super.initState();
-  }
-
-Future _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print("Handling a background message: ${message.messageId}");
-}
-
-void registerNotification() async {
-  // 1. Initialize the Firebase app
-  await Firebase.initializeApp();
-
-  // 2. Instantiate Firebase Messaging
-  _messaging = FirebaseMessaging.instance;
-  // Add the following line
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-  // 3. On iOS, this helps to take the user permissions
-  NotificationSettings settings = await _messaging.requestPermission(
-    alert: true,
-    badge: true,
-    provisional: false,
-    sound: true,
-  );
-
-  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-    print('User granted permission');
-// For handling the received notifications
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      // Parse the message received
       PushNotification notification = PushNotification(
         title: message.notification?.title,
         body: message.notification?.body,
@@ -128,24 +128,11 @@ void registerNotification() async {
         _notificationInfo = notification;
         _totalNotifications++;
       });
-
-      // ...
-      if (_notificationInfo != null) {
-        // For displaying the notification as an overlay
-        showSimpleNotification(
-          Text(_notificationInfo!.title!),
-          leading: NotificationBadge(totalNotifications: _totalNotifications),
-          subtitle: Text(_notificationInfo!.body!),
-          background: Colors.cyan.shade700,
-          duration: Duration(seconds: 2),
-        );
-      }
-
     });
-  } else {
-    print('User declined or has not accepted permission');
+
+    super.initState();
   }
-}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -167,29 +154,28 @@ void registerNotification() async {
           SizedBox(height: 16.0),
           NotificationBadge(totalNotifications: _totalNotifications),
           SizedBox(height: 16.0),
-          // TODO: add the notification text here
           _notificationInfo != null
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'TITLE: ${_notificationInfo!.dataTitle ?? _notificationInfo!.title}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16.0,
-                  ),
-                ),
-                SizedBox(height: 8.0),
-                Text(
-                  'BODY: ${_notificationInfo!.dataBody ?? _notificationInfo!.body}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ],
-            )
-          : Container(),
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'TITLE: ${_notificationInfo!.dataTitle ?? _notificationInfo!.title}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'BODY: ${_notificationInfo!.dataBody ?? _notificationInfo!.body}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                )
+              : Container(),
         ],
       ),
     );
